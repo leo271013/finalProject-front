a4a626e42de52054c5aefd83646ef3c386e0a0d7
leo271013@gmail.com <leo271013@gmail.com>
2022-03-01 16:02:02 +0800

    try

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,13 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: ["vuetify"],
 });
+
+module.exports = {
+  publicPath: "./",
+  chainWebpack(config) {
+    config.plugin("html").tap((args) => {
+      args[0].title = "Swapper";
+      return args;
+    });
+  },
+};
